66d9094a27bacc43ede561bbd4ae8cd2071d7a18
Nithin <nithinsgayathri@gmail.com>
2024-05-25 13:37:27 +0530

    Remove CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import router from './routes/url.route.js';
 import cors from 'cors';
-import { corsOptions } from './config/cors.config.js';
+// import { corsOptions } from './config/cors.config.js';
 
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
