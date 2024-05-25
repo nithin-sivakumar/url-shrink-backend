21977c39fb35bbee4429f15b71a5a909c22d39e2
Nithin <nithinsgayathri@gmail.com>
2024-05-25 13:26:21 +0530

    Update CORS

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,11 +1,11 @@
-// const whitelist = ['http://localhost:5000', 'http://localhost:5173', '*'];
+const whitelist = ['https://url-shrink-frontend.vercel.app'];
 
-// export const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// };
+export const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
